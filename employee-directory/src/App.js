50a815d2618b44ebf66bed5d3e994ba36f7db2b4
Melissa.Kinsey <kinsey.melissa@gmail.com>
2021-03-30 07:56:31 -0500

    files: Add untracked file from last commit.

---
+++ employee-directory/src/App.js
@@ -0,0 +1,11 @@
+import Nav from './components/Nav/Nav.jsx'
+function App()
+{
+  return (
+    <div className="App">
+      <Nav/>
+    </div>
+  );
+}
+
+export default App;
---
+++ employee-directory/src/components/Nav/Nav.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Nav = () =>
+{
+    return (
+        <nav className="navbar sticky-top navbar-light bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">Sticky top</a>
+        </div>
+      </nav>
+  )  
+}
+
+export default Nav
